4c9adbcd23010693724b00531186c9fd64c66982
Argonaut <gaming.genos1729@gmail.com>
2021-05-31 17:27:56 +0530

    Update intro.js

--- commands/intro.js
+++ commands/intro.js
@@ -1,24 +1,24 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.token = process.env.BOTTOKEN;
-const devid = process.env.ownerid;
-const camp_name = "Ɖɨʋɨռɛ";
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.token = process.env.BOTTOKEN
+const devid = process.env.ownerid
+const camp_name = "Ɖɨʋɨռɛ"
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = "0123456789ABCDEF"
+  var color = "#"
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
 }
 module.exports = async function (msg) {
-    const dev = await client.users.fetch(devid);
-    let mssg = `Thanks for you're concern.\nI am a bot developed by **${dev.tag}** to help our campmates here in **${camp_name}**. Any trouble, DM me.`;
-    const embed = new Discord.MessageEmbed()
-        .setColor(getRandomColor())
-        .setTitle('Introduction')
-        .setDescription(mssg)
-        .setTimestamp();
-    msg.reply(embed)
+  const dev = await client.users.fetch(devid)
+  let mssg = `Thanks for you're concern.\nI am a bot developed by **${dev.tag}** to help our campmates here in **${camp_name}**. Any trouble, DM me.`
+  const embed = new Discord.MessageEmbed()
+    .setColor(getRandomColor())
+    .setTitle("Introduction")
+    .setDescription(mssg)
+    .setTimestamp()
+  msg.reply(embed)
 }
